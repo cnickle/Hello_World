6c1368fa8b6e81f67282496e8a06b51dfdf7140f
Cameron <camnickle@gmail.com>
2020-02-26 16:37:21 -0500

    Finishing up the HATNA molecule

--- penquins/class_sciData.py
+++ penquins/class_sciData.py
@@ -12,8 +12,9 @@ import matplotlib.pyplot as plt
 import scipy.optimize as sco
 import numpy as np
 import warnings
-import random
+from numpy.random import random
 import math
+import time
 from penquins.class_gen import gen as genAlg
 
 class sciData:
@@ -29,63 +30,69 @@ class sciData:
 #                           ))**2)
 #        print(standarderror)
     
-    def evolutionary(self, parBnds, popSize, genSize):
-        popObj = genAlg(parBnds,popSize)
+    # def evolutionary(self, parBnds, popSize, genSize):
+    #     popObj = genAlg(parBnds,popSize)
         
-        for gen in range(genSize):
-            weights = []
-            pop = popObj.nextgen(weights)
-            for count in pop.keys():
-                w = self.calcRelativeError(pop[count])
-                print('\t%.3f'%w)
-                weights += [w]
+    #     for gen in range(genSize):
+    #         weights = []
+    #         pop = popObj.nextgen(weights)
+    #         for count in pop.keys():
+    #             w = self.calcRelativeError(pop[count])
+    #             print('\t%.3f'%w)
+    #             weights += [w]
             
-            print('Gen %d Avg: %.3f'%(gen,np.mean(weights)))
-            np.reciprocal(weights)
-            if gen > 0:
-                for name in popObj.parameters.keys():
-                    avg = np.mean(popObj.parameters[name])
-                    std = np.std(popObj.parameters[name])
-                    lower = avg-3*std
-                    higher = avg+3*std
-                    print('%s\t[%f,%f]'%(name,lower,higher))
+    #         print('Gen %d Avg: %.3f'%(gen,np.mean(weights)))
+    #         np.reciprocal(weights)
+    #         if gen > 0:
+    #             for name in popObj.parameters.keys():
+    #                 avg = np.mean(popObj.parameters[name])
+    #                 std = np.std(popObj.parameters[name])
+    #                 lower = avg-3*std
+    #                 higher = avg+3*std
+    #                 print('%s\t[%f,%f]'%(name,lower,higher))
             
             
 
 # %% Output Data and Plots
-    def parRange(self, initpar, par, ran, size = 10, method = 'relative'):        
+    def parRange(self, initpar, par, ran, size = 10, method = 'linear', plot = False):        
         pars = initpar.copy()
         step = (ran[1]-ran[0])/size
         t = np.arange(ran[0],ran[1],step)
         
+        if plot:
+            plt.figure()
+            plt.scatter(self.rawdat['X'],self.rawdat['Y'],color = 'black')
+        
         output = ''
         for i in t:
             pars[par]=i
-            
+            start = time.time()
             if method == 'relative':
                 Err = self.calcRelativeError(pars)
-            else:
+            if method == 'linear':
                 Err = self.calcLinearError(pars)
-                
+            end = time.time()
+            diff = np.floor(end-start)
             for name in pars.keys():
                 output = output + '%e\t'%pars[name]
-            output = output + '%.3e\n'%Err
+            output = output + '%.5e\t%.5e\t%.5e\n'%(Err, diff, np.sqrt(Err**2+diff**2))
             f= open('Test.txt',"a")
             f.write(output)
             f.close()
-            if Err < 0.01:
-                print('%.3e\t%.3e'%(i,Err))
-            else:
-                print('%.3e\t%.3f'%(i,Err))
             
-    def returnThry(self, xvals, initpar=[]):
-        if not initpar:
-            initpar = self.parameters.copy()
-        yThr = self.model(xvals, *initpar.values())
-        dat = {'X': xvals,
-               'Y': yThr}
-        return dat
+            print('%.3e\t%.3f\t%.3f\t%.3f'%(i,Err*100, diff, np.sqrt((Err*100)**2+diff**2)))
+            
+            if plot:
+                plt.plot(self.modelDat['X'], self.modelDat['Y'], label = i, linewidth=4)
+            self.modelDat={}
+                
+        
+        if plot:
+            plt.legend()
     
+
+
+# %% Tested and Reliable Functions
     def subsetData(self, xRange = []):
         X = list(self.rawdat['X'])
         Y = list(self.rawdat['Y'])
@@ -105,8 +112,14 @@ class sciData:
         self.workingdat['Y'] = newY
         
         return newX,newY
-
-# %% Tested and Reliable Functions
+    
+    def returnThry(self, initpar=[]):
+        
+        if self.modelDat:
+            return self.modelDat['X'], self.modelDat['Y']
+        else:
+            return self.rawdat['X'], self.model(self.rawdat['X'], *initpar.values())    
+    
     def __init__(self,fName, equation, rawdat = {}):
         self.fitbool = False
         self.scaling = False
@@ -122,12 +135,13 @@ class sciData:
         else:
             self.rawdat = rawdat
         
-        minval = min(abs(self.rawdat['Y']))
+        minval = min(abs(np.nonzero(self.rawdat['Y'])[0]))
         if minval < np.sqrt(np.finfo(float).eps):
             # Due to curve_fit and machine precision limitations the data and
             # model are being scaled into the nano range. This should not 
             # effect the fitting parameters
             warnings.warn("Scaling data and equation due to floating point percision")
+            print(minval)
             self.scaling = True
             self.multiplier = 1E9
             self.rawdat['Y'] = self.rawdat['Y']*self.multiplier
@@ -176,6 +190,13 @@ class sciData:
     
     def fit(self,parBnds, parInitial):
         self.fitbool = True
+        
+        if parBnds:
+            for parm in parBnds.keys():
+                if not (parInitial[parm] < parBnds[parm][1] and parInitial[parm] >parBnds[parm][0]):
+                    print('x0 is infeasible. Choosing random number')
+                    parInitial[parm] = parBnds[parm][0] + (random()*(parBnds[parm][1]-parBnds[parm][0]))
+        
         results,covar  = self.__fit(self.model,parBnds,parInitial)
         
         self.modelDat['X']=self.workingdat['X']
@@ -186,7 +207,25 @@ class sciData:
             self.parameters[name] = results[cnt]
             self.errors[name]  = np.sqrt(np.diag(covar))[cnt]
             cnt +=1
-
+            
+    def customFit(self,func,parInitial):
+        self.fitbool = True
+        
+        def minfunc(x):
+            yThr = self.model(self.workingdat['X'],*x)
+            return func(self.workingdat['Y'],yThr)
+        
+        res = sco.minimize(minfunc,x0 = list(parInitial.values()))
+        
+        self.modelDat['X']=self.workingdat['X']
+        self.modelDat['Y']=self.model(self.workingdat['X'], *res.x)
+        
+        cnt = 0
+        for name in list(parInitial.keys()):
+            self.parameters[name] = res.x[cnt]
+            self.errors[name]  = 0
+            cnt +=1
+            
     def __rangeOrder(self, order):
         switchCases={
                 'atto'      :   18,
@@ -214,11 +253,12 @@ class sciData:
         if self.modelDat:
             Ythr = self.modelDat['Y']
         else:
-            Ythr = self.model(X,*initpar.values())
+            self.modelDat['X'] = self.workingdat['X']
+            self.modelDat['Y'] = self.model(X,*initpar.values())
             
-        residual = np.subtract(Y,Ythr)
+        residual = np.subtract(Y,self.modelDat['Y'])
         Error = np.sqrt(np.sum(residual**2))
-        return Error
+        return Error/len(X)
     
     def calcRelativeError(self, initpar):
         X = self.workingdat['X']
@@ -233,8 +273,11 @@ class sciData:
         Error = np.sqrt(np.sum(residual**2))
         return Error
     
-    def printFit(self,save = ''):
-        Err = self.calcRelativeError(self.parameters)
+    def printFit(self,save = '', relError = False):
+        if not relError:
+            Err = self.calcLinearError(self.parameters)
+        else:
+            Err = self.calcRelativeError(self.parameters)
         output = "Fit Report:\tError:\t%.2f\n" % Err
         output = output + "\tPar:\tVal\tErr\n"
         
